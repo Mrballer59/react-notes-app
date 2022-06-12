8ead44a04e71ecb897eac296293ed54efecbcd7b
B <77552838+Mrballer59@users.noreply.github.com>
2022-06-12 22:56:21 +0200

    display done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import NotesList from "./components/NotesList";
+import { useState } from ".react";
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    { id: nanoid(), text: "This is my first note", date: "15/05/2005" },
+  ]);
   return (
     <div className="container">
       <NotesList />
