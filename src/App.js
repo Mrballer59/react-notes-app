18b96b54dc635785b7e7449e354682c53ef8792d
B <77552838+Mrballer59@users.noreply.github.com>
2022-06-13 23:07:50 +0200

    added to fix errof

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import NotesList from "./components/NotesList";
 import { useState } from ".react";
+import { nanoid } from "nanoid";
 
 const App = () => {
   const [notes, setNotes] = useState([
     { id: nanoid(), text: "This is my first note", date: "15/05/2005" },
+    { id: nanoid(), text: "this is my second note!!", date: "15/04/2021" },
   ]);
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes={notes} />
     </div>
   );
 };
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,11 +1,10 @@
 import Note from "./Note";
-const NotesList = () => {
+const NotesList = ({ notes }) => {
   return (
     <div className="notes-list">
-      <Note />
-      <Note />
-      <Note />
-      <Note />
+      {notes.map((note) => (
+        <Note />
+      ))}
     </div>
   );
 };
