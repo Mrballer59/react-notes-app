97faabec68341fb05b13c327c0a7264ddb35f54f
B <77552838+Mrballer59@users.noreply.github.com>
2022-06-18 14:31:25 +0100

    notes and log added

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,14 +1,23 @@
+import { useState } from "react";
+
 const AddNote = () => {
+  const [noteText, setNotesText] = useState("");
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <div className="note new">
       <textarea
         rows="8"
         cols="10"
         placeholder="Type to add a note..."
+        onChange={handleChange}
       ></textarea>
       <div className="note-footer">
         <small>200 Remaining </small>
+        <button className="save">Save</button>
       </div>
     </div>
   );
 };
+export default AddNote;
